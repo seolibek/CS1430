852c52b984b617861a00806083746d1c061390a2
Lisa Baek <110780435+seolibek@users.noreply.github.com>
2024-12-05 12:15:16 -0500

    SAM synthetic data gen

--- preprocessing.py
+++ preprocessing.py
@@ -12,19 +12,5 @@ uh yeah this should totally be colab i think
 
 '''
 
-#first download the pretrained SAM model? https://github.com/yuval-alaluf/SAM
-# yeah this should totally be a notebook ill change it after
-mkdir pretrained_models
-pip install gdown
-gdown "https://drive.google.com/u/0/uc?id=1XyumF6_fdAxFmxpFcmPf-q84LU_22EMC&export=download" -O pretrained_models/sam_ffhq_aging.pt
-wget "https://github.com/italojs/facial-landmarks-recognition/raw/master/shape_predictor_68_face_landmarks.dat"
-
-#Having trained your model or if you're using a pretrained SAM model, you can use scripts/inference.py to run inference on a set of images.
-python scripts/inference.py \
---exp_dir=/path/to/experiment \
---checkpoint_path=experiment/checkpoints/best_model.pt \
---data_path=/path/to/test_data \
---test_batch_size=4 \
---test_workers=4 \
---couple_outputs
---target_age= 18, 23, 28, 33, 38, 43, 48, 53, 58, 63, 68, 73, 78, 85
+'tweaking we can j copy this from SAM. see: inference_playground.ipynb
+'
