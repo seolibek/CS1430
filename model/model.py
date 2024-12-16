b463c4f5869bdb64fbd609c2ee1da35af3a0f540
Lisa Baek <110780435+seolibek@users.noreply.github.com>
2024-12-15 22:49:57 -0500

    final code

--- model/model.py
+++ model/model.py
@@ -101,11 +101,11 @@ class PatchGANDiscriminator(nn.Module):
         super(PatchGANDiscriminator, self).__init__()
         self.model = nn.Sequential(
             nn.Conv2d(input_channels, 64, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.LeakyReLU(0.3, inplace=True),
 
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.LeakyReLU(0.3, inplace=True),
 
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(256),
